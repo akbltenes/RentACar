498d01d4b438528cf4cef26401af36c214e0b2da
Enes PC <akbltenes638@gmail.com>
2025-02-24 03:43:11 +0300

    Kod temizlendi

--- rentACar/src/main/java/enes/akblt/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/enes/akblt/rentACar/webApi/controllers/BrandsController.java
@@ -15,38 +15,21 @@ import enes.akblt.rentACar.entities.concretes.Brand;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@RestController //restcontroller varsa bura erisim noktası
-@RequestMapping("/api/brands") //adresleme
+@RestController 
+@RequestMapping("/api/brands") 
 @AllArgsConstructor
 @NoArgsConstructor
 public class BrandsController {
-// dataaccess constructurla businesin icinde kullanıldı
-//businessi api cagıracak
+
 	private BrandService brandService;
 
-@Autowired //bu anotasyou kullanma amacımız sudur.
-//parametreli constructorun içindeki parametreye bak.
-//parametredeki classa git.BrandServiceyi kim implements ediyorsa
-//onun newlenmiş halini bana ver.
-//public BrandsController(BrandService brandService) {//
-	//super();//
-	//this.brandService = brandService; burası yrine de constructoru lombokla olusturalım
-//}
+@Autowired 
+
 	
 @GetMapping("getall")
 public List<GetAllBrandResponse> getAll(){
 	return brandService.getAll();
-//www.akblt.com diye bi web sitesi olsun
-//www.akblt.com/api/brands/getall demek getmappingi çağır demektir.
-//bu anotasyonun içindeki brandsevirce.getAll calısır.
-//buradaki brandService businesstaki BrandManager'a gidecek
-//BrandServiceye gitmez cunku o interface.//BrandManagera gidince
-//BrandManager iş kurallarından geçecek.
-//iş kurallarından geçerse InMemoryBrandRepositorye gidecek
-//çünkü BrandManager classından geçerse InMemoryBrandRepositorye
-//opsiyonlanıyor.
-//inmemoryde de getAll oldugu için inMemory markalaır listeleyecek
-	
+
 }
 @PostMapping("/add")
 public void add(CreateBrandRequest createrBrandRequest) {
